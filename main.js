3b44753822d937451d36d03a24fc36add606a1ca
AidanDeMuth <aidanspace87@gmail.com>
2024-05-20 08:24:15 -0500

    server

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ import latexOutput from './latexOutput.js';
 import {gcd, frac} from './frac.js';
 import vector from './vector.js';
 
-const selectOperation = () => {
+selectOperation = () => {
 	var operation = document.getElementById('operation').value;
 	console.log(operation);
-}
+	console.log("hihihihi");
 
-switch (choice) {
+	let choice = 0;
+
+	switch (choice) {
 	case 0: // Row reduction
 		matrixOperations.reducedRowEchelon(matrix, 0, 0);
 		latexOutput.matrixLatex(matrix);
@@ -49,4 +51,42 @@ switch (choice) {
 		}
 		matrixOperations.printMatrix(gram);
 		break;
+	}
 }
+
+checkValue = (element) => {
+        let min = element.min;
+        let max = element.max;
+    
+        let value = parseInt(element.value);
+        if (value > max) {
+          element.value = max;
+        } 
+        else if (value < min) {
+          element.value = min;
+        }
+      }
+
+getTable = () => {
+    document.getElementById('Table Space').innerHTML = '';
+    let num_rows = document.getElementById('Rows').value;
+    let num_cols = document.getElementById('Columns').value;
+    let table = document.createElement('Table')
+    table.style.margin = 'auto';
+
+    for (let x = 0; x < num_rows; x++) {
+        let row = table.insertRow();
+        for (let y = 0; y < num_cols; y++) {
+        	let cell = row.insertCell();
+        	let input_box = document.createElement('input');
+        	input_box.style.width = '50px'
+
+        	cell.appendChild(input_box);
+        	cell.style.border = '1px solid black';
+
+        	console.log('test');
+        }
+    }
+
+    document.getElementById('Table Space').appendChild(table);
+} 
